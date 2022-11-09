94129465f166ce418096fd6412c780631898b4c8
Macai, Mario <mario.macai@accenture.com>
2022-11-09 08:58:18 +0100

    fixed lint issues

--- mock_api/server.py
+++ mock_api/server.py
@@ -1,10 +1,12 @@
+"""Mock API for the Go-eCharger wallbox"""
+
 from flask import Flask, request
 
 app = Flask(__name__)
 
 SUPPORTED_CAR_CHANGE_VALUES = ["amp", "frc", "alw", "psm", "acs"]
 
-car = {
+CAR = {
     "car": 1,
     "sse": "051215",
     "amp": 10,
@@ -24,25 +26,31 @@ car = {
 
 @app.route("/api/status", methods=["GET"])
 def car_status() -> dict:
-    global car
-    return car
+    """
+    Return mocked car status.
+    """
+    return CAR
 
 
 @app.route("/api/set", methods=["GET"])
 def car_set() -> dict:
-    global car
+    """
+    Update mocked car status.
+    """
+    # pylint: disable=global-variable-not-assigned
+    global CAR
     args = request.args
 
     for change_value in SUPPORTED_CAR_CHANGE_VALUES:
         request_value = args.get(change_value, None)
-        if request_value != None:
-            car[change_value] = (
+        if request_value is not None:
+            CAR[change_value] = (
                 bool(int(request_value))
                 if change_value == "alw"
                 else int(request_value)
             )
 
-    return car
+    return CAR
 
 
 if __name__ == "__main__":
